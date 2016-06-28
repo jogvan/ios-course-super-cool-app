b4215b1c3473465904b7b69c2a17281dbaff25c7
Jógvan Højgaard <SSD@Jogvans-MacBook-Pro.local>
2016-06-28 22:05:16 +0100

    added files for supercool

---
+++ SuperCool/SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Jógvan Højgaard on 28-06-16.
+//  Copyright © 2016 Jógvan Højgaard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
